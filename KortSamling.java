8a2dd1355c091addf2216ef3a1b063605f552258
h594754 <37376894+h594754@users.noreply.github.com>
2021-10-16 22:46:42 +0200

    Ferdig oppgave 1
    Kjørt JUnit test uten feil.

---
+++ KortSamling.java
@@ -0,0 +1,218 @@
+package no.hvl.dat100.prosjekt.modell;
+
+import no.hvl.dat100.prosjekt.TODO;
+import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
+
+/**
+ * Struktur for å lagre ei samling kort. Kan lagre hele kortstokken. Det finnes
+ * en konstant i klassen Regler som angir antall kort i hver av de 4 fargene. Når
+ * programmet er ferdig settes denne til 13, men under utvikling / testing kan
+ * det være praktisk å ha denne mindre.
+ * 
+ */
+public class KortSamling {
+
+	public static int MAKS_KORT = 4 * Regler.MAKS_KORT_FARGE;
+
+	private Kort[] samling;
+	private int antall;
+
+	/**
+	 * Oppretter en tom Kortsamling med plass til MAKS_KORT (hele kortstokken).
+	 */
+	public KortSamling() {
+		
+
+		samling = new Kort[MAKS_KORT];
+	}
+
+	/**
+	 * Returnerer en tabell med kortene i samlinga. Tabellen trenger ikke være
+	 * full. Kortene ligger sammenhengende fra starten av tabellen. Kan få
+	 * tilgang til antallet ved å bruke metoden getAntallKort(). Metoden er
+	 * først og fremst ment å brukes i testklasser. Om man trenger
+	 * kortene utenfor, anbefales metoden getAlleKort().
+	 * 
+	 * @return tabell av kort.
+	 */
+	public Kort[] getSamling() {
+		
+		return samling;
+		
+	}
+	
+	/**
+	 * Antall kort i samlingen.
+	 * 
+	 * @return antall kort i samlinga.
+	 */
+	public int getAntalKort() {
+		
+		int antall = 0; 
+		for(int i=0; i<samling.length;i++) {
+			if(samling[i] != null) {
+				antall += 1;
+			}
+		}
+		return antall;
+
+	}
+	
+	/**
+	 * Sjekker om samlinga er tom.
+	 * 
+	 * @return true om samlinga er tom, false ellers.
+	 */
+	public boolean erTom() {
+						
+		boolean tom = false; 
+		if(samling[0] == null) {
+			tom = true;
+		}
+		return tom; 
+		
+	}
+
+	/**
+	 * Legg et kort til samlinga.
+	 * 
+	 * @param kort
+	 *            er kortet som skal leggast til.
+	 */
+	public void leggTil(Kort kort) {
+				
+		for(int i=0; i<samling.length;i++) {
+			if(samling[i] == null) {
+				samling[i] = kort; 
+				break;
+			}
+		}		 		
+	}
+	
+	/**
+	 * Legger alle korta (hele kortstokken) til samlinga. Korta vil være sortert
+	 * slik at de normalt må stokkes før bruk.
+	 */
+	public void leggTilAlle() {
+		// Husk: bruk Regler.MAKS_KORT_FARGE for å få antall kort per farge			
+	for(Kortfarge farge : Kortfarge.values()) {
+		for(int i=1; i<=Regler.MAKS_KORT_FARGE; i++) {
+			leggTil(new Kort(farge, i));
+			
+			}
+		}
+	}
+
+
+	/**
+	 * Fjerner alle korta fra samlinga slik at den blir tom.
+	 */
+	public void fjernAlle() {
+		
+		for(int i = 0; i < samling.length; i++) {
+			samling[i] = null; 
+		}
+	}
+	
+	/**
+	 * Ser på siste kortet i samlinga.
+	 * 
+	 * @return siste kortet i samlinga, men det blir ikke fjernet. Dersom samalinga er tom, returneres
+	 *         null.
+	 */
+	public Kort seSiste() {
+		
+		Kort siste = samling[samling.length-1];
+		if(samling[0] == null) {
+			return null;
+		}
+		
+		for(int i = 0; i < samling.length; i++) {
+			if(samling[i] == null && i !=0) {
+				siste = samling[i-1];
+				break;
+			}
+		}
+			return siste; 
+		}
+
+	/**
+	 * Tek ut siste kort fra samlinga.
+	 * 
+	 * @return siste kortet i samlinga. Dersom samalinga er tom, returneres
+	 *         null.
+	 */
+	public Kort taSiste() {
+		
+		Kort siste = samling[samling.length-1];
+		if(samling[0] == null) {
+			return null;
+		}
+		
+		for(int i = 0; i < samling.length; i++) {
+			if(samling[i] == null && i !=0) {
+				siste = samling[i-1];
+				samling[i-1] = null;
+				break;
+			}
+		}
+			return siste; 
+		}
+	
+	/**
+	 * Undersøker om et kort finst i samlinga.
+	 * 
+	 * @param kort.
+	 * 
+	 * @return true om kortet finst i samlinga, false ellers.
+	 */
+	public boolean har(Kort kort) {
+		boolean har = false;
+		if(kort != null) {
+			for(int i = 0; i < samling.length; i++) {
+			if(samling[i] == kort) {
+				har = true; 
+			}
+		} 
+	} return har;
+}
+
+	/**
+	 * Fjernar et kort frå samlinga. Dersom kortet ikke finnest i samlinga,
+	 * skjer ingenting med samilingen
+	 * 
+	 * @param kort
+	 *            kortet som skal fjernast. Dersom kortet ikke finnes, skjer
+	 *            ingenting.
+	 * @return true om kortet blev fjernet fra samlinga, false ellers.
+	 */
+			 
+	public boolean fjern(Kort kort) {
+		boolean fjern = false;
+		if (kort != null) {
+			for(int i = 0; i < samling.length; i++) {
+			if(kort == samling[i] ) {
+				samling[i] = null;
+				fjern = true; 
+			}
+		} return fjern;
+	} else {
+		return fjern;
+	}
+}
+	/**
+	 * Gir kortene som en tabell av samme lengde som antall kort i samlingen
+	 * 
+	 * @return tabell av kort som er i samlingen, der kort skal ha samme rekkefølge
+	 *         som i kortsamlinga.
+	 */
+	public Kort[] getAlleKort() {
+		Kort[] alle = new Kort[getAntalKort()];
+		for(int i = 0; i < getAntalKort(); i++) {
+			alle[i] = samling[i];
+		}
+		return alle;
+	} 
+}
+	
+
